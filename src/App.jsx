f5636fdaa45c906d118cf31137295b7855cac283
janexmgd <janexmgd@gmail.com>
2024-11-15 12:44:50 +0700

    adding detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,19 @@
-import { useState } from "react";
-import { ChevronUp } from "lucide-react";
-import nikahFixLogo from "../src/assets/NIKAHFIX.webp";
-import guestIcon from "../src/assets/Guest.webp";
-import echidnaImage from "../src/assets/echidna.jpg";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from '../src/pages/HomePage.jsx';
+import DetailPage from '../src/pages/DetailPage.jsx';
 
-export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    window.alert("hahaha kasian wibu halu");
-  };
-
+const App = () => {
   return (
-    <div className="flex w-full bg-black h-svh text-white">
-      {!isOpen ? (
-        <div className="mt-16 flex flex-col w-full items-center max-h-svh">
-          <img src={nikahFixLogo} alt="NikahFix Logo" className="w-32 h-auto" />
-          <p className="mt-16 font-sans text-lg">Who's Watching</p>
-          <div
-            className="mt-10 hover:scale-125 flex flex-col items-center transition-transform duration-200 cursor-pointer"
-            onClick={() => setIsOpen(true)}
-          >
-            <img src={guestIcon} alt="Guest Icon" className="w-24 h-auto" />
-            <p className="mt-2">Guest</p>
-          </div>
-          <div className="mt-16 uppercase text-xs text-gray-500 outline outline-1 outline-gray-500 p-1">
-            CLICK YOUR NAME
-          </div>
-        </div>
-      ) : (
-        <div className="flex w-full flex-col items-center cursor-not-allowed h-svh">
-          <div className="w-full max-w-md mx-auto h-full flex flex-col">
-            <div
-              className="relative w-full flex-grow bg-cover bg-center flex flex-col justify-end"
-              style={{ backgroundImage: `url(${echidnaImage})` }}
-            >
-              <div className="absolute inset-0 bg-gradient-to-t from-black via-black/70 to-transparent" />
-              <div className="relative z-10 flex flex-col justify-end p-4">
-                <img
-                  src={nikahFixLogo}
-                  alt="NikahFix Logo"
-                  className="w-20 h-auto mb-4"
-                />
-                <p className="text-2xl font-sans font-bold">Denny & Echidna:</p>
-                <p className="text-2xl font-sans font-bold mb-4">
-                  Sebelum Hari H
-                </p>
-                <div className="mb-4">
-                  <span className="rounded-md text-xs font-sans p-1 bg-[#E50913] font-bold">
-                    Coming soon
-                  </span>
-                  <span className="text-white text-sm font-medium ml-3">
-                    24 Desember 2024
-                  </span>
-                </div>
-                <div className="flex flex-wrap mb-2">
-                  {[
-                    "romantic",
-                    "getmarried",
-                    "animegirls",
-                    "echidnabestgirls",
-                  ].map((tag) => (
-                    <span
-                      key={tag}
-                      className="bg-[#4D4D4D] py-1 px-2 rounded-full text-xs text-white mr-2 mb-2"
-                    >
-                      #{tag}
-                    </span>
-                  ))}
-                </div>
-                <div
-                  className="mt-2 flex flex-col items-center cursor-pointer pb-8"
-                  onClick={handleClick}
-                >
-                  <ChevronUp className="w-12 h-12 animate-bounce" />
-                  <span className="text-lg font-bold font-sans">
-                    SEE THE DETAIL
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    <Router>
+      <div className='bg-black min-h-screen'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/detail' element={<DetailPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
+
+export default App;
---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,440 @@
+import { useEffect, useState, useRef } from 'react';
+import video from '../assets/prewed-echidna.mp4';
+import echidnaHeader from '../assets/echidna-header.webp';
+import ReactPlayer from 'react-player';
+import netflixLogo from '../assets/netflix.png';
+import logo4k from '../assets/4k.webp';
+import lovely from '../assets/lovely.jpg';
+import echidnaImage from '../assets/echidna.jpg';
+import GuestIcon from '../assets/Guest.webp';
+const DetailPage = () => {
+  let listComment = [
+    {
+      name: 'Emilia',
+      message: 'Ihh sayangku denny kok ninggalin aku siii',
+    },
+    {
+      name: 'Rem',
+      message: 'Ihh sayangku denny kok ninggalin aku siii',
+    },
+    {
+      name: 'Priscillia',
+      message: 'Ihh sayangku denny kok ninggalin aku siii',
+    },
+    {
+      name: 'Crush',
+      message: 'Ihh sayangku denny kok ninggalin aku siii',
+    },
+    {
+      name: 'Mimi',
+      message: 'Ihh sayangku denny kok ninggalin aku siii',
+    },
+    {
+      name: 'Garf',
+      message: 'Selamat bahagia bos',
+    },
+    {
+      name: 'Al',
+      message: 'semoga bahagia bro',
+    },
+    {
+      name: 'Ram',
+      message: 'Barusu >_<',
+    },
+    {
+      name: 'Beatrice',
+      message: 'Apcb',
+    },
+  ];
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [comments, setComments] = useState(listComment);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    listComment.push({
+      name,
+      message,
+    });
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    window.alert('success send wish');
+    setComments(listComment);
+    setName('');
+    setMessage('');
+    setIsSubmitting(false);
+  };
+  const scrollCommentRef = useRef(null);
+  useEffect(() => {
+    if (scrollCommentRef.current) {
+      scrollCommentRef.current.scrollTop =
+        scrollCommentRef.current.scrollHeight;
+    }
+  }, [comments]);
+  return (
+    <div className='flex w-full bg-black text-white pb-4 cursor-not-allowed'>
+      <div className='w-full max-w-md mx-auto flex flex-col relative flex-grow'>
+        <div className='top-0 left-0 right-0'>
+          <ReactPlayer
+            url={video}
+            loop={true}
+            playing={true}
+            width='100%'
+            height='auto'
+            style={{ aspectRatio: '16 / 9' }}
+          />
+        </div>
+        <div className='mt-4 px-4'>
+          <div className='flex flex-wrap font-sans'>
+            <img src={netflixLogo} alt='NikahFix Logo' className='w-4 h-auto' />
+            <span className='font-sans pl-1 text-gray-500 text-xs font-medium uppercase'>
+              Documenter
+            </span>
+          </div>
+          <div className='flex flex-col mt-2'>
+            <div className='text-white text-xl font-sans font-bold'>
+              Denny & Echidna:Nikah Ama Anime
+            </div>
+            <div className='flex items-center'>
+              <span className='text-green-500'>100% match</span>
+              <span className='bg-slate-500 text-xs ml-2 px-1 py-0 rounded-sm'>
+                SU
+              </span>
+              <span className='ml-2'>2024 1h 26m</span>
+              <img src={logo4k} alt='logo4k' className='ml-2' />
+              <img src={logo4k} alt='logo4k' className='ml-2' />
+            </div>
+            <div className='flex flex-col mt-2'>
+              <span className='bg-[#E50913] px-2 py-0 max-w-fit flex items-center justify-center rounded-sm'>
+                Coming soon on Tuesday, 24 October 2024
+              </span>
+              <div className='text-white mt-2'>
+                Setelah Denny Dan Echidna dipertemukan di isekai dalam kondisi
+                tepat, di mana keduanya telah siap untuk memulai hubungan
+                bersama, tibalah mereka di awal perjalanan baru menuju
+                pernikahan
+              </div>
+              <div className='mt-2 text-xs text-gray-500'>
+                "Segala sesuatu Kami ciptakan berpasang-pasangan agar kamu
+                mengingat (kebesaran Allah)" (Q.S Az-Zariyah: 49)
+              </div>
+            </div>
+            <div className='mt-6'>
+              <header className='text-lg font-bold'>Breaking News</header>
+              <img
+                src={echidnaHeader}
+                alt=''
+                srcSet=''
+                style={{ aspectRatio: '16 / 9' }}
+                className='mt-2 rounded-md'
+              />
+              <div className='text-sm italic pt-2 text-gray-500'>
+                <p className='mt-2'>
+                  Halo! Karena kalian adalah orang penting yang mengisi
+                  hari-hari kami, kami ingin informasikan bahwa kami akan segera
+                  menikah!
+                </p>
+                <p className='mt-4'>
+                  Tapi sebelumnya, kami mohon maaf kepada teman dan kerabat
+                  semua karena tidak bisa mengundang kalian hadir di hari
+                  bahagia kami, dikarenakan pernikahan kami bersifat intimate
+                  wedding yang dilaksanakan di Bekasi dan hanya dihadiri oleh
+                  keluarga dan orang terdekat.
+                </p>
+                <p className='mt-4'>
+                  Walaupun begitu, kami harapkan sebaik-baiknya doa untuk
+                  kelancaran pernikahan dan hari-hari bahagia setelahnya.
+                </p>
+                <p className='mt-4'>
+                  Dengan penuh cinta,
+                  <br />
+                  The bride and groom
+                </p>
+              </div>
+            </div>
+            <div className='mt-6'>
+              <header className='text-lg font-bold'>Bride and Groom</header>
+              <div className='grid grid-cols-2 w-full gap-4'>
+                <div className='flex flex-col'>
+                  <img
+                    src={lovely}
+                    alt=''
+                    srcSet=''
+                    style={{ aspectRatio: '16 / 9' }}
+                    className='mt-2 rounded-2xl'
+                  />
+                  <div className='flex flex-col mt-2'>
+                    <span className='text-white'>Echidna</span>
+                    <span className='text-sm text-gray-500 mt-2'>
+                      Witch of Greed
+                    </span>
+                  </div>
+                </div>
+                <div className='flex flex-col'>
+                  <img
+                    src={lovely}
+                    alt=''
+                    srcSet=''
+                    style={{ aspectRatio: '16 / 9' }}
+                    className='mt-2 rounded-2xl'
+                  />
+                  <div className='flex flex-col mt-2'>
+                    <span className='text-white'>Echidna</span>
+                    <span className='text-sm text-gray-500 mt-2'>
+                      Witch of Greed
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='mt-6'>
+              <header className='text-lg font-bold'>Our Love Story</header>
+              <div className='grid grid-cols-1 gap-4'>
+                {/* Episode1 */}
+                <div className='flex flex-col'>
+                  <div className='grid grid-cols-2 gap-5'>
+                    <img
+                      src={lovely}
+                      alt=''
+                      srcSet=''
+                      style={{ aspectRatio: '16 / 9' }}
+                      className='mt-2 rounded-2xl'
+                    />
+                    <div className='flex flex-col justify-center'>
+                      <div className='text-white'>
+                        Episode 1: How We Meet Each Other that Time
+                      </div>
+                      <div className='text-[#A3A1A1]  text-sm mt-2'>
+                        26m 10s
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='text-[#A3A1A1] text-sm'>
+                  Denny dan Echidna pertama kali bertemu di isekai. Karena
+                  sering memiliki kepentingan yang berbahya, Echidna jadi sering
+                  bertemu dengan Denny
+                </div>
+                <div className='flex flex-col'>
+                  <div className='grid grid-cols-2 gap-5'>
+                    <img
+                      src={lovely}
+                      alt=''
+                      srcSet=''
+                      style={{ aspectRatio: '16 / 9' }}
+                      className='mt-2 rounded-2xl'
+                    />
+                    <div className='flex flex-col justify-center'>
+                      <div className='text-white'>
+                        Episode 2: A Love That Grows With Time
+                      </div>
+                      <div className='text-[#A3A1A1]  text-sm mt-2'>
+                        26m 10s
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='text-[#A3A1A1]  text-sm'>
+                  Hari demi Hari terlewati rasa cinta pun tumbuh diantara mereka
+                  berdua
+                </div>
+                <div className='flex flex-col'>
+                  <div className='grid grid-cols-2 gap-5'>
+                    <img
+                      src={lovely}
+                      alt=''
+                      srcSet=''
+                      style={{ aspectRatio: '16 / 9' }}
+                      className='mt-2 rounded-2xl'
+                    />
+                    <div className='flex flex-col justify-center'>
+                      <div className='text-white'>
+                        Episode 3: Choose to Spend Life Together
+                      </div>
+                      <div className='text-[#A3A1A1]  text-sm mt-2'>
+                        26m 10s
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='text-[#A3A1A1] text-sm'>
+                  Seringkali Denny memberitahu kepada Echidan bahwa ia tertarik
+                  dan ingin menjalanin Hubungan lebih serius
+                </div>
+                <div className='flex flex-col'>
+                  <div className='grid grid-cols-2 gap-5'>
+                    <img
+                      src={lovely}
+                      alt=''
+                      srcSet=''
+                      style={{ aspectRatio: '16 / 9' }}
+                      className='mt-2 rounded-2xl'
+                    />
+                    <div className='flex flex-col justify-center'>
+                      <div className='text-white'>
+                        [Coming Soon] Final Episode: The Beginning of Forever
+                      </div>
+                      <div className='text-[#A3A1A1]  text-sm mt-2'>
+                        26m 10s
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='text-[#A3A1A1]  text-sm'>
+                  Kasihan wibu halu dik
+                </div>
+              </div>
+            </div>
+            <div className='mt-6'>
+              <header className='text-lg font-bold'>Our Love Gallery</header>
+              <div className='grid grid-cols-3 gap-3'>
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+                <img
+                  src={echidnaImage}
+                  alt=''
+                  srcSet=''
+                  style={{ aspectRatio: '9 / 16' }}
+                  className='rounded-2xl hover:scale-125 transition-all duration-300 ease-in-out'
+                />
+              </div>
+            </div>
+            <div className='mt-6'>
+              <header className='text-lg font-bold'>Wish for the couple</header>
+              <div
+                className='max-h-[55svh] overflow-y-auto mt-2 bottom-0'
+                ref={scrollCommentRef}
+              >
+                {comments.map((item, i) => (
+                  <div key={i} className='flex pt-2'>
+                    <div className='flex items-center'>
+                      <img
+                        src={GuestIcon}
+                        alt=''
+                        srcSet=''
+                        className='w-6 h-auto'
+                      />
+                    </div>
+                    <div className='flex flex-col ps-2 justify-between items-start w-full h-full text-sm pt-2'>
+                      <div className='text-white text-sm'>{item.name}</div>
+                      <div className='text-gray-400'>{item.message}</div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <form onSubmit={handleSubmit} className='space-y-4 mt-4 relative'>
+                <div className='pb-2'>
+                  <label htmlFor='' className='mb-2 font-sans'>
+                    Name
+                  </label>
+                  <input
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className='w-full mt-4 p-2 text-black font-sans'
+                    disabled={isSubmitting}
+                  />
+                </div>
+                <div>
+                  <label htmlFor='' className='mb-2 font-sans'>
+                    Message
+                  </label>
+                  <textarea
+                    value={message}
+                    aria-expanded={false}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className='w-full mt-4 text-black p-2 font-sans'
+                    disabled={isSubmitting}
+                  />
+                </div>
+                <button
+                  type='submit'
+                  className='w-full bg-black'
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? 'Submitting...' : 'Submit'}
+                </button>
+              </form>
+            </div>
+            <div className='mt-6 text-center text-sm'>
+              <span className=''>
+                Thank you for checking up all the things up there!
+              </span>
+              <br />
+              <span>Can’t wait to see u again!</span>
+            </div>
+            <div className='mt-12 text-xs text-center text-[#A3A1A1]'>
+              Cloning with ❤️ by janexmgd & supported by Echidna
+            </div>
+            <div className='mt-5 text-xs cursor-pointer text-center mb-5'>
+              <a
+                href='https://www.nikahfix.com'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                thx nikahfix.com
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailPage;
